97e833b088893582b1b6ca50134eec75572d6d47
CJ Avilla <cjavilla@gmail.com>
2015-04-07 09:12:39 -0700

    update to pg

--- db/schema.rb
+++ db/schema.rb
@@ -13,6 +13,9 @@
 
 ActiveRecord::Schema.define(version: 20150330161443) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "comments", force: :cascade do |t|
     t.integer  "photo_id"
     t.string   "body"
